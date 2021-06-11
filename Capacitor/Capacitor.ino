f52dac3be79ab34e91800bcdb49d30a9db8d4841
Rico <code@ricoip.com>
2021-06-11 17:12:39 +0800

    =Oops I don't have my GPG key on hand ;)

---
+++ Capacitor/Capacitor.ino
@@ -0,0 +1,64 @@
+#include <Capacitor.h> // Capacitor library
+#include <CapacitorLite.h>
+#include <Servo.h> // Servo library
+
+/* Notes:
+YELLOW = Signal
+RED = 5V Power
+BROWN = Ground */
+int servoPinA = 3; // Servo PWM pin @ D3
+int servoPinB = 5;
+Servo ServoA;
+Servo ServoB;
+
+Capacitor capA(9, A5); // Capacitor @ D8 and A5
+Capacitor capB(11, A3);
+
+void setup() {
+  Serial.begin(9600);
+  ServoA.attach(servoPinA, 500, 2450);
+  ServoB.attach(servoPinB, 500, 2450);
+}
+
+
+void loop() {
+  float capAOutput = capA.Measure();
+  float capBOutput = capB.Measure();
+  int rotateAngleA = servoRotateA(capAOutput);
+  int rotateAngleB = servoRotateB(capBOutput);
+  ServoA.write(rotateAngleA);
+  ServoB.write(rotateAngleB);
+
+  Serial.print("CapA: ");
+  Serial.print(capAOutput);
+  Serial.print(", ");
+  Serial.print("RotateA: ");
+  Serial.print(rotateAngleA);
+  Serial.print(", ");
+  Serial.print("CapB: ");
+  Serial.print(capBOutput);
+  Serial.print(", ");
+  Serial.print("RotateB: ");
+  Serial.println(rotateAngleB);
+  
+  /*
+  Serial.print(capAOutput);
+  Serial.print(",");
+  Serial.print(rotateAngleA);
+  Serial.print(",");
+  Serial.print(capBOutput);
+  Serial.print(",");
+  Serial.println(rotateAngleB);
+  */
+  delay(250);
+}
+
+int servoRotateA(int input) {
+  int angle = map(input, 350, 150, 0, 120);
+  return angle;
+}
+
+int servoRotateB(int input) {
+  int angle = map(input, 150, 350, 0, 120);
+  return angle;
+}
